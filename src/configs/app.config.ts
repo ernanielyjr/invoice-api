6874b49d1768a83b336039c8647645caf2576610
Ernani Ely Jr <ernani.ely@softdesign-rs.com.br>
2018-08-02 15:01:11 -0300

    pre heroku

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,7 +1,8 @@
 const appConfig = {
   passKey: '8C1DFCE96682D7472406DF6C2F29EFD9F71E39F06EEA71DB564CBC496E4C19C6',
   database: {
-    uri: 'mongodb://127.0.0.1:27017/myplan-api',
+    uri: 'mongodb://myplan:mYpl75$nAp1@ds263161.mlab.com:63161/myplan-api',
+    // uri: 'mongodb://127.0.0.1:27017/myplan-api',
   },
   jwt: {
     secret: 'D6DC9BECDD295E9663AF19F889C8F0CD91B5C30970C47EB4897A9A4824C4C1CD',
