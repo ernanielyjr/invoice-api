80a6d33633252ec0e88ac58c0ff1d8b0a9c0bf56
Ernani Ely Jr <ernani.ely@softdesign-rs.com.br>
2018-10-17 22:15:03 -0300

    Route for first invoice fixed

--- src/api/invoice/invoice.routes.ts
+++ src/api/invoice/invoice.routes.ts
@@ -3,7 +3,6 @@ import InvoiceController from './invoice.controller';
 class InvoiceRoutes {
   public config(app): void {
     app.route('/api/v1/invoice/close').get(InvoiceController.closeAllInvoices.bind(InvoiceController));
-    app.route('/api/v1/invoice/first/:customerId').get(InvoiceController.customerFirstInvoice.bind(InvoiceController));
 
     app.route('/api/v1/invoice').get(InvoiceController.get.bind(InvoiceController));
     app.route('/api/v1/invoice').post(InvoiceController.create.bind(InvoiceController));
