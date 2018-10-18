45fc65de0a54bd371486a3441e8d84962da3b530
Ernani Ely Jr <ernani.ely@softdesign-rs.com.br>
2018-10-17 22:25:58 -0300

    Rollback first invoice routes

--- src/api/customer/customer.routes.ts
+++ src/api/customer/customer.routes.ts
@@ -1,5 +1,4 @@
 import CustomerController from './customer.controller';
-import InvoiceController from '../invoice/invoice.controller';
 
 class CustomerRoutes {
   public config(app): void {
@@ -12,7 +11,6 @@ class CustomerRoutes {
     app.route('/api/v1/customer/:id/services').get(CustomerController.listServices.bind(CustomerController));
     app.route('/api/v1/customer/:id/invoices').get(CustomerController.listInvoices.bind(CustomerController));
     app.route('/api/v1/customer/:id/current-invoice').get(CustomerController.currentInvoice.bind(CustomerController));
-    app.route('/api/v1/customer/:customerId/first-invoice').get(InvoiceController.customerFirstInvoice.bind(InvoiceController));
   }
 }
 
